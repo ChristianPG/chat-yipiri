1c1da92d0b665010458285141d81396cafbc0447
Christian Patiño <christianpg813@gmail.com>
2026-03-27 16:33:54 -0500

    feature: Add socket connection between server and client

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const port = process.env.PORT ?? 3000;
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', socket => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat message', msg => {
+    console.log('User sent', msg);
+    io.emit('chat message', msg);
+  });
+});
 
 app.get('/', (_req, res) => {
   res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
