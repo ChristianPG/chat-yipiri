1bd0dbfa6e0ee9266b3637d121820c58a768bb8d
Christian Patiño <christianpg813@gmail.com>
2026-03-27 17:18:58 -0500

    feature: Save messages in a relational database

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+import { createClient, type ResultSet } from '@libsql/client';
+
+dotenv.config();
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+const db = createClient({
+  url: process.env.DB_URL ?? '',
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`);
 
 io.on('connection', socket => {
   console.log('User connected');
@@ -15,9 +33,19 @@ io.on('connection', socket => {
     console.log('User disconnected');
   });
 
-  socket.on('chat message', msg => {
-    console.log('User sent', msg);
-    io.emit('chat message', msg);
+  socket.on('chat message', async message => {
+    let result: ResultSet;
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:message)',
+        args: { message },
+      });
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+
+    io.emit('chat message', message, result.lastInsertRowid?.toString());
   });
 });
 
